41773510cb8c72c63aa5626c3fa6112310e184b5
Sergey Kabanets <skabanets97@gmail.com>
2024-08-28 00:14:58 +0300

    Add gradient overlay to hero section image

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,6 +13,7 @@ const Hero = (props: HeroProps) => {
     <div className="relative h-screen">
       <div className="absolute -z-10 inset-0">
         <Image src={imgData} alt={imgAlt} fill style={{ objectFit: "cover" }} />
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-900"></div>
       </div>
       <div className="pt-48 flex justify-center items-center">
         <h1 className="text-white text-6xl">{title}</h1>
