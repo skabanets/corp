1ddb7d577960c3911d6be5a6267ea5594977ef4c
Sergey Kabanets <skabanets97@gmail.com>
2024-08-27 19:45:33 +0300

    Add navigation to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
-
 const HomePage = () => {
-  return (
-    <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
-      Home Page
-    </div>
-  );
+  return <div>Home Page</div>;
 };
 
 export default HomePage;
