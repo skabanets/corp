4cbe8434c3ddfd681cc065347271ce4a83b97378
Sergey Kabanets <skabanets97@gmail.com>
2024-08-27 19:34:01 +0300

    Add links for page navigation on Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
+      Home Page
+    </div>
+  );
 };
 
 export default HomePage;
